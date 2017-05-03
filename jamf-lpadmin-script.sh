398a29f90ce7ba17519b826289cf4a6dd4436cfb
John Singer <jpsthecelt@gmail.com>
2017-05-03 16:31:30 -0700

    Initial submission
    Created this script to test pushing through JAMF (casper).

---
+++ jamf-lpadmin-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# jpsinger-5.3.17 jamf-lpadmin-script.sh, assumes that it's run on a MAC, using lpadmin & lpstat
+# Sample script I created to demonstrate the use of:
+# A) lpadmin for a pharos printer
+# use of exit-code
+# use of lpstat command
+lpadmin -p "MyNewPharosQueueName" -E -D "PharosDisplayName" -v popup://PharosPrintServer/QueueName -o printer-is-shared=false -P "/Library/Printers/PPDs/Contents/Resources/RICOH_Pro_C4503.ppd"
+echo "The LPADMIN returned status is: " $?
+lpstat -p
